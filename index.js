246ddfee04efca891f2a0d277a23bb9117645847
Laynaria <pro.laynaria@outlook.fr>
2024-06-17 12:35:19 +0200

    added json use and a first route

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
